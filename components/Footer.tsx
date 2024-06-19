f600761e2b333f5c382a8de74bc89ee83a4ffbb0
Kevin Baluyot <baluyotkevin33@yahoo.com>
2024-06-19 12:04:59 -0700

    portfolio

---
+++ components/Footer.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
+import { socialMedia } from '@/data'
+
+const Footer = () => {
+  return (
+    <footer className="w-full pb-10 mb-[100px] md:mb-5" id="contact">
+        <div className="flex flex-col items-center">
+            <h1 className="heading lg:max-w-[45vw] ">
+                <span className="text-red">Contact</span> Me!
+            </h1>
+            <a href="mailto:kevincbaluyot33@gmail.com">
+                <MagicButton 
+                    title="Let's get in touch"
+                    icon={<FaLocationArrow />}
+                    position="right"
+                />
+            </a>
+        </div>
+
+        <div className="flex mt-16 md:flex-row flex-col justify-between items-center" >
+            <p className="md:text-base text-sm md:font-normal font-light">Copyright © 2024 Kevin Baluyot</p>
+
+            <div className="flex items-center md:gap-3 gap-6">
+                {socialMedia.map((profile) => (
+                    <a
+                        className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300" 
+                        key={profile.id}
+                        href={profile.link}
+                        target="_blank"
+                        >
+                        <img
+                            src={profile.img}
+                            alt={profile.id}
+                            width={20}
+                            height={20}
+                            />
+                    </a>
+                ))}
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ data/index.ts
@@ -0,0 +1,147 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Dancing Reading Guitar Cooking Fitness Drawing Writing Singing",
+    description: "Leisure Hours",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I love engaging with new individuals and building relationships",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "A passion for developing innovative solutions",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building with NextJS and React Native",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "EventMeet",
+    des: "Create your own events and purchase tickets to attend other events!",
+    img: "/eventmeet.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stripe.svg", "/c.svg"],
+    link: "https://event-app-bay-kappa.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "MLS",
+    des: "MLS (My Love Story), the ultimate destination for expressing and exploring love in every shape and form.",
+    img: "/MLS.png",
+    iconLists: ["/re.svg", "/js.svg", "/python.svg", "/css.svg", "/redux.svg"],
+    link: "https://mlscapstone.onrender.com/",
+  },
+  {
+    id: 3,
+    title: "taskPanda",
+    des: "An application that allows users to hire taskers in order for their tasks to be completed",
+    img: "/taskPanda.jpeg",
+    iconLists: ["/re.svg", "/js.svg", "/python.svg", "/css.svg", "/redux.svg"],
+    link: "https://github.com/Mattchu18/AllFourYou",
+  },
+];
+
+export const aboutMe = [
+  {
+    id: 1,
+    title: "Always a Student",
+    desc: "Fast self-learner from a young age, constantly exploring and learning about the various subjects I'm passionate about.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Tech Stack",
+    desc: "HTML, CSS, Git, JavaScript, Python, Flask, SQLAlchemy, NextJS, React, SQL, TypeScript, Express, NodeJS, Docker, AWS, C#, Java, Stripe, Postgres, MongoDB, React Native, Redux, Cypress, Socket.IO, SQLite",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "My Motto",
+    desc: "Work hard, practice kindness consistently, and allow love to always be your guiding force.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Full Stack Developer",
+    desc: "Developed and maintained user-facing features using modern technologies, optimized database queries, and ensured a smooth, seamless user interface.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Baluyotkevin"
+  },
+  {
+    id: 2,
+    img: "/insta.svg",
+    link: "https://www.instagram.com/kebuhbuh"
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/kevin-baluyot-2102b5bb/"
+  },
+];
+
+export const description = ["Full Stack Developer", "Professional Dancer"]
